022b1a7bc64dfc8661025c1b74f9f074dfbf379a
Beisenbek Baisakov <bbaisakov@dar.kz>
2022-02-05 17:58:50 +0600

    upd

--- w2/part 2/3.py
+++ w2/part 2/3.py
@@ -3,17 +3,28 @@ items = input().split()
 projection = map(int, items)
 a = list(projection) 
 '''
-def f(a, c):
-    if(c >= len(a)):
-         return "1"
+memory = []
 
-    for i in range(1, a[c] + 1):
-        if f(a, c + i) == "1":
-            return "1"
-    
-    return "0"
+def f(a, c):
+    if memory[c] == -1:
+        if(c >= len(a) - 1): memory[c] = 1
+        else:
+            found = False
+            for i in range(1, a[c] + 1):
+                if f(a, c + i) == 1:
+                    memory[c] = 1
+                    found = True
+                    break
+
+            if not found: memory[c] = 0
+        
+    return memory[c]
+
 
 a = list(map(int, input().split()))
+
+for i in range(len(a)):
+    memory.append(-1)
 
 print(f(a, 0))
     
