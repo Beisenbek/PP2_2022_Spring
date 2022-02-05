9407f0c798cef6faf71eb0c5de01a774f80a3df2
Beisenbek Baisakov <bbaisakov@dar.kz>
2022-02-05 18:06:05 +0600

    upd

--- w2/part 2/3.py
+++ w2/part 2/3.py
@@ -3,23 +3,20 @@ items = input().split()
 projection = map(int, items)
 a = list(projection) 
 '''
+
+#recursion with memorization
 memory = []
 
 def f(a, c):
     if memory[c] == -1:
         if(c >= len(a) - 1): memory[c] = 1
         else:
-            found = False
+            memory[c] = 0
             for i in range(1, a[c] + 1):
                 if f(a, c + i) == 1:
                     memory[c] = 1
-                    found = True
                     break
-
-            if not found: memory[c] = 0
-        
     return memory[c]
-
 
 a = list(map(int, input().split()))
 
