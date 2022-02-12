ee5d92caae9c3f24c0f36d58709c0972270f754c
Beisenbek Baisakov <bbaisakov@dar.kz>
2022-02-12 13:27:36 +0600

    week3 upd

--- w3/18.py
+++ w3/18.py
@@ -2,9 +2,28 @@ d = {}
 n = int(input())
 for i in range(n):
     objs = input().split()
-    d[objs[0]] = objs[1]
+    if objs[1] in d:
+        d[objs[1]] += 1
+    else:
+        d[objs[1]] = 1
 
-m = int(input())
 h = {}
+m = int(input())
+for i in range(m):
+    objs = input().split()
+    if objs[1] in h:
+        h[objs[1]] += int(objs[2])
+    else:
+        h[objs[1]] = int(objs[2])
 
-print(d)
+for w in d:
+    if w in h:
+        if d[w] > h[w]: d[w] -= h[w]
+        else: d[w] = 0
+
+res = 0
+
+for w in d:
+    res += d[w]
+
+print("Demons left: " + str(res))
